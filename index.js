d1deb3048a950f919257a76ddc3274eb315a62fc
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2024-11-22 09:14:24 +0530

    feat(connection): Establish mongoose mongodb node.js connection

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-// to import mongoose 
+// To import mongoose 
 const mongoose = require('mongoose');
+
+// Establish mongoose mongodb node.js connection
+
+const mongoose = async () => {
+    try{
+        const conn = await mongoose.connect(`mongodb://loacalhost:27017/test`, {
+            useNewUrlParser:true,
+        });
+        console.log(`MongoDB Connected: {conn.connection.host}`);
+    } catch(error){
+        console.error(error.message);
+        process.exit(1);
+    }
+}
+
 
 app.get('/', (req, res) => {
     console.log('I am inside home page route handler');
