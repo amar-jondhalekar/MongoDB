5dbe7af8cd48d7ec8dd37ecd1730d03e3255b5a5
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2024-11-22 09:21:38 +0530

    feat(remove) : removed db function

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-// To import mongoose 
-const mongoose = require('mongoose');
-
-// Establish mongoose mongodb node.js connection
-
-const connectDB = async () => {
-    try{
-        const conn = await mongoose.connect(`mongodb+srv://amarjondhalekar221297:jYMGGazQCVRySIS5@cluster0.bazyj.mongodb.net/`, {
-            useNewUrlParser:true,
-        });
-        console.log(`MongoDB Connected`);
-    } catch(error){
-        console.error(error.message);
-        process.exit(1);
-    }
-}
 
 
 app.get('/', (req, res) => {
