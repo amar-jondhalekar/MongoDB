31886823a9691bcf57fbd2092b6418e1760bb20f
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2024-11-22 09:08:14 +0530

    feat(mongoose): imported mongoose

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+// to import mongoose 
+const mongoose = require('mongoose');
 
 app.get('/', (req, res) => {
     console.log('I am inside home page route handler');
