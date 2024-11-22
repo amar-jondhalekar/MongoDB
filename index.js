242fb8576b2e872ea2eb912b88584b8962037c1c
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2024-11-22 09:25:25 +0530

    feat(DBconnect): connected DB to database

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const connectDB = require('./db');
 
+// connect to database 
+connectDB();
 
 app.get('/', (req, res) => {
     console.log('I am inside home page route handler');
