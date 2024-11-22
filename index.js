bdf9f180a03ae38c1f7a57fd9b703fc0627a4e90
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2024-11-22 09:06:02 +0530

    feat(server): created server file using express server

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    console.log('I am inside home page route handler');
+    res.send("Hello, Welcome to Campuslight");
+})
+
+
+app.listen(PORT, () => {
+    console.log('Server is Up');
+})
